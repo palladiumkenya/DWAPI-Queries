0a11fa0c103c08fe30a6e065d0e8a127ed2aa2d8
Koske Kimutai <koskedk@live.com>
2021-03-04 16:09:05 +0300

    fixed quotes

--- DWAPI New Extracts/Patient status extract.sql
+++ DWAPI New Extracts/Patient status extract.sql
@@ -11,13 +11,13 @@ select
        --CAST(now() as Date) AS DateExtracted,
        max(disc.visit_date) AS ExitDate,
        mid(max(concat(visit_date,(case discontinuation_reason
-                                    when 159492 then "Transfer Out"
-                                    when 160034 then "Died"
-                                    when 5240 then "LTFU"
-                                    when 819 then "Cannot afford Treatment"
-                                    when 5622 then "Other"
-                                    when 1067 then "Unknown"
-                                    else "" end), "" )),20) as ExitReason,
+                                    when 159492 then 'Transfer Out'
+                                    when 160034 then 'Died'
+                                    when 5240 then 'LTFU'
+                                    when 819 then 'Cannot afford Treatment'
+                                    when 5622 then 'Other'
+                                    when 1067 then 'Unknown'
+                                    else '' end), '' )),20) as ExitReason,
        (case mid(max(concat(date(disc.visit_date),disc.trf_out_verified)),11) when 1065 then 'Yes' when 1066 then 'No' end) as TOVerified,
        mid(max(concat(date(disc.visit_date),disc.trf_out_verification_date)),11) as TOVerifiedDate,
        disc.date_created as Date_Created,
